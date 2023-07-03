9db2d4e302efe9e7584481516cf1749e21b4ebce
YoussefDarwishh <youssefdarwishh@gmail.com>
2023-07-03 16:42:07 +0300

    Add project files.

---
+++ RenderRSS/Pages/Index.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace RenderRSS.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<FeedItem> FeedItems { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var xmlContent = await FetchXmlContentAsync("http://scripting.com/rss.xml");
+            FeedItems = ParseXmlContent(xmlContent);
+
+            return Page();
+        }
+
+        private async Task<string> FetchXmlContentAsync(string url)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                return await httpClient.GetStringAsync(url);
+            }
+        }
+
+        private List<FeedItem> ParseXmlContent(string xmlContent)
+        {
+            var feedItems = new List<FeedItem>();
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xmlContent);
+
+            XmlNodeList channelNodes = doc.GetElementsByTagName("channel");
+            XmlNodeList itemNodes = channelNodes[0].SelectNodes("item");
+
+            foreach (XmlNode itemNode in itemNodes)
+            {
+                FeedItem feedItem = new FeedItem();
+
+                feedItem.Title = itemNode.SelectSingleNode("title")?.InnerText ?? string.Empty;
+                feedItem.Description = itemNode.SelectSingleNode("description")?.InnerText ?? string.Empty;
+                feedItem.PubDate = DateTime.Parse(itemNode.SelectSingleNode("pubDate")?.InnerText ?? string.Empty);
+                feedItem.Link = itemNode.SelectSingleNode("link")?.InnerText ?? string.Empty;
+
+                feedItems.Add(feedItem);
+            }
+            return feedItems;
+        }
+    }
+
+    public class FeedItem
+    {
+        public string ? Title { get; set; }
+        public string ? Description { get; set; }
+        public DateTime ? PubDate { get; set; }
+        public string ? Link { get; set; }
+    }
+}
