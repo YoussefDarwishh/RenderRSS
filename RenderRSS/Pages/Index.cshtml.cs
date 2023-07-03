1ecc3b38c3013522b67438852f147a51343f3339
YoussefDarwishh <110547843+YoussefDarwishh@users.noreply.github.com>
2023-07-03 16:49:43 +0300

    Update Index.cshtml.cs

--- RenderRSS/Pages/Index.cshtml.cs
+++ RenderRSS/Pages/Index.cshtml.cs
@@ -52,8 +52,8 @@ namespace RenderRSS.Pages
     public class FeedItem
     {
         public string ? Title { get; set; }
-        public string ? Description { get; set; }
-        public DateTime ? PubDate { get; set; }
-        public string ? Link { get; set; }
+        public string  Description { get; set; }
+        public DateTime  PubDate { get; set; }
+        public string  Link { get; set; }
     }
 }
